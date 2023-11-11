b62fb5161cf8bfe764bee96f01791db1a4592948
a-reliable-chair <71333221+a-reliable-chair@users.noreply.github.com>
2023-11-11 16:54:00 -0500

    Firestarter Functional

--- project_lose.ltproj/resources/custom_components/custom_skill_components.py
+++ project_lose.ltproj/resources/custom_components/custom_skill_components.py
@@ -10,6 +10,32 @@ from app.engine.game_state import game
 from app.engine.objects.unit import UnitObject
 from app.utilities import utils, static_random
 
+class StartAndEndEventInitiate(SkillComponent):
+    nid = 'start_and_end_event_initiate'
+    desc = 'Calls events before and after combat initated by user'
+    tag = SkillTags.CUSTOM
+
+    expose = (ComponentType.NewMultipleOptions)
+    options = {
+        "start_event": ComponentType.Event,
+        "end_event": ComponentType.Event,
+    }
+    
+    def __init__(self, value=None):
+        self.value = {
+            "start_event": '',
+            "end_event": '',
+        }
+        if value:
+            self.value.update(value)
+
+    def start_combat(self, playback, unit, item, target, mode):
+        if mode == 'attack':
+            game.events.trigger_specific_event(self.value.get('start_event'), unit, target, unit.position, {'item': item, 'mode': mode})
+    
+    def end_combat(self, playback, unit: UnitObject, item, target: UnitObject, mode):
+        if mode == 'attack':
+            game.events.trigger_specific_event(self.value.get('end_event'), unit, target, unit.position, {'item': item, 'mode': mode})
 
 class DoNothing(SkillComponent):
     nid = 'do_nothing'
